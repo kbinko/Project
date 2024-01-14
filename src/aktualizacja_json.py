41bbeba637c3e86ac39cc8d4b26de389707097df
Kbinko <59764236+kbinko@users.noreply.github.com>
2024-01-14 19:35:16 +0000

    aktualizacja zawartości etykiet

--- src/aktualizacja_json.py
+++ src/aktualizacja_json.py
@@ -1,10 +1,12 @@
 import json
 import os
 
-def aktualizuj_pliki_json(folder):
+def aktualizuj_pliki_json(folder, start, stop):
     for nazwa_pliku in os.listdir(folder):
-        if nazwa_pliku.endswith('.json'):
-            sciezka_pliku = os.path.join(folder, nazwa_pliku)
+        if nazwa_pliku.endswith(".json"):
+            numer_zdjecia = int(nazwa_pliku.split('_')[1].split('.')[0])
+            if start <= numer_zdjecia <= stop:
+                sciezka_pliku = os.path.join(folder, nazwa_pliku)
 
             with open(sciezka_pliku, 'r') as plik:
                 dane = json.load(plik)
@@ -13,5 +15,6 @@ def aktualizuj_pliki_json(folder):
             with open(sciezka_pliku, 'w') as plik:
                 json.dump(zaktualizowane_dane, plik)
 
+
 folder_etykiet = '../data/etykiety'  # Zastąp ścieżką do folderu zawierającego pliki JSON
-aktualizuj_pliki_json(folder_etykiet)
+aktualizuj_pliki_json(folder_etykiet, 151, 300)
